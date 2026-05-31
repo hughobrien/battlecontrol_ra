14c80e03bf9edba0469a48a6fc7652024923db99
Hugh O'Brien <github@hughobrien.ie>
2026-05-30 23:03:10 -0700

    port: define MS path limit constants (#84)

--- linux-compat/win32-shim/direct.h
+++ linux-compat/win32-shim/direct.h
@@ -4,6 +4,22 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#ifndef _MAX_DRIVE
+#define _MAX_DRIVE 3
+#endif
+#ifndef _MAX_DIR
+#define _MAX_DIR 256
+#endif
+#ifndef _MAX_FNAME
+#define _MAX_FNAME 256
+#endif
+#ifndef _MAX_EXT
+#define _MAX_EXT 256
+#endif
+#ifndef _MAX_PATH
+#define _MAX_PATH 260
+#endif
+
 #ifndef _chdir
 #define _chdir chdir
 #endif
