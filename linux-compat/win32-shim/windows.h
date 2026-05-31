0146484e6a33e4b2afcdab111e139bf372ead4d9
Hugh O'Brien <mail@hughobrien.ie>
2026-05-30 21:51:16 -0700

    port: wire cnc-ddraw headers

---
+++ linux-compat/win32-shim/windows.h
@@ -0,0 +1,180 @@
+#ifndef BATTLECONTROL_RA_WIN32_SHIM_WINDOWS_H
+#define BATTLECONTROL_RA_WIN32_SHIM_WINDOWS_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef int BOOL;
+typedef unsigned char BOOLEAN;
+typedef unsigned char BYTE;
+typedef uint16_t WORD;
+typedef uint32_t DWORD;
+typedef uint64_t DWORD64;
+typedef int64_t LONGLONG;
+typedef uint64_t ULONGLONG;
+typedef long LONG;
+typedef unsigned long ULONG;
+typedef int INT;
+typedef unsigned int UINT;
+typedef uintptr_t UINT_PTR;
+typedef intptr_t LONG_PTR;
+typedef uintptr_t ULONG_PTR;
+typedef uintptr_t DWORD_PTR;
+typedef wchar_t WCHAR;
+typedef void VOID;
+
+typedef void *HANDLE;
+typedef void *HWND;
+typedef void *HINSTANCE;
+typedef void *HMODULE;
+typedef void *HDC;
+typedef void *HBITMAP;
+typedef void *HCURSOR;
+typedef void *HICON;
+typedef void *HMENU;
+typedef void *HMONITOR;
+typedef void *HPALETTE;
+typedef void *HHOOK;
+typedef void *HKEY;
+typedef void *HGDIOBJ;
+
+typedef char *LPSTR;
+typedef const char *LPCSTR;
+typedef WCHAR *LPWSTR;
+typedef const WCHAR *LPCWSTR;
+typedef void *LPVOID;
+typedef const void *LPCVOID;
+typedef BOOL *LPBOOL;
+typedef BYTE *LPBYTE;
+typedef WORD *LPWORD;
+typedef DWORD *LPDWORD;
+typedef LONG *LPLONG;
+
+typedef LONG HRESULT;
+typedef UINT_PTR WPARAM;
+typedef LONG_PTR LPARAM;
+typedef LONG_PTR LRESULT;
+
+typedef struct tagRECT {
+    LONG left;
+    LONG top;
+    LONG right;
+    LONG bottom;
+} RECT, *LPRECT;
+
+typedef struct tagPOINT {
+    LONG x;
+    LONG y;
+} POINT, *LPPOINT;
+
+typedef struct tagSIZE {
+    LONG cx;
+    LONG cy;
+} SIZE, *LPSIZE;
+
+typedef union _LARGE_INTEGER {
+    struct {
+        DWORD LowPart;
+        LONG HighPart;
+    };
+    LONGLONG QuadPart;
+} LARGE_INTEGER;
+
+typedef struct _GUID {
+    DWORD Data1;
+    WORD Data2;
+    WORD Data3;
+    BYTE Data4[8];
+} GUID, IID, CLSID;
+typedef GUID *LPGUID;
+typedef const GUID *REFGUID;
+typedef const IID *REFIID;
+typedef const CLSID *REFCLSID;
+
+typedef struct tagPALETTEENTRY {
+    BYTE peRed;
+    BYTE peGreen;
+    BYTE peBlue;
+    BYTE peFlags;
+} PALETTEENTRY, *LPPALETTEENTRY;
+
+typedef struct _RGNDATA {
+    DWORD dwSize;
+    BYTE Buffer[1];
+} RGNDATA, *LPRGNDATA;
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+#ifndef FAR
+#define FAR
+#endif
+#ifndef NEAR
+#define NEAR
+#endif
+#ifndef PASCAL
+#define PASCAL
+#endif
+#ifndef WINAPI
+#define WINAPI
+#endif
+#ifndef CALLBACK
+#define CALLBACK
+#endif
+#ifndef APIENTRY
+#define APIENTRY
+#endif
+#ifndef __cdecl
+#define __cdecl
+#endif
+#ifndef __stdcall
+#define __stdcall
+#endif
+#ifndef __success
+#define __success(x)
+#endif
+
+#ifndef MAKE_HRESULT
+#define MAKE_HRESULT(sev, fac, code) \
+    ((HRESULT)(((uint32_t)(sev) << 31) | ((uint32_t)(fac) << 16) | (uint32_t)(code)))
+#endif
+
+#ifndef DEFINE_GUID
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+    static const GUID name = { (DWORD)(l), (WORD)(w1), (WORD)(w2), { (BYTE)(b1), (BYTE)(b2), (BYTE)(b3), (BYTE)(b4), (BYTE)(b5), (BYTE)(b6), (BYTE)(b7), (BYTE)(b8) } }
+#endif
+
+#ifndef DECLARE_HANDLE
+#define DECLARE_HANDLE(name) typedef void *name
+#endif
+
+#ifndef FAILED
+#define FAILED(hr) (((HRESULT)(hr)) < 0)
+#endif
+#ifndef SUCCEEDED
+#define SUCCEEDED(hr) (((HRESULT)(hr)) >= 0)
+#endif
+#ifndef S_OK
+#define S_OK ((HRESULT)0)
+#endif
+#ifndef E_FAIL
+#define E_FAIL ((HRESULT)0x80004005L)
+#endif
+#ifndef E_INVALIDARG
+#define E_INVALIDARG ((HRESULT)0x80070057L)
+#endif
+#ifndef E_OUTOFMEMORY
+#define E_OUTOFMEMORY ((HRESULT)0x8007000EL)
+#endif
+#ifndef E_NOTIMPL
+#define E_NOTIMPL ((HRESULT)0x80004001L)
+#endif
+
+#endif
