3136edd958c7a273f51a703a967139e029cc6459
Hugh O'Brien <github@hughobrien.ie>
2026-05-31 00:25:47 -0700

    port: compile file and DDE support objects (#95)

--- linux-compat/win32-shim/dos.h
+++ linux-compat/win32-shim/dos.h
@@ -1,9 +1,28 @@
 #ifndef BATTLECONTROL_RA_WIN32_SHIM_DOS_H
 #define BATTLECONTROL_RA_WIN32_SHIM_DOS_H
 
+#ifndef _HARDERR_FAIL
+#define _HARDERR_FAIL 0
+#endif
+
+#ifndef _A_SUBDIR
+#define _A_SUBDIR 0x10
+#endif
+
+#ifndef _MAX_PATH
+#define _MAX_PATH 260
+#endif
+
+struct find_t {
+    unsigned attrib;
+    char name[_MAX_PATH];
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+int _dos_findfirst(const char *filespec, unsigned attrib, struct find_t *fileinfo);
 
 static inline int _dos_getdrive(unsigned int *drive)
 {
