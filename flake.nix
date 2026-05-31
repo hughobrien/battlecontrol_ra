4e36af47fc80a4b2945307e7af60348bda2c70cf
Hugh O'Brien <github@hughobrien.ie>
2026-05-30 21:39:33 -0700

    port: add minimal nix and zig build entrypoints (#70)
    * port: add minimal nix flake
    
    * port: add minimal zig build entrypoint

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Zig build environment for battlecontrol_ra";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      packages = with pkgs; [
+        zig
+      ];
+      buildApp = pkgs.writeShellApplication {
+        name = "battlecontrol-ra-zig-build";
+        runtimeInputs = packages;
+        text = ''
+          exec zig build "$@"
+        '';
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        inherit packages;
+      };
+
+      apps.${system} =
+        let
+          build = {
+            type = "app";
+            program = "${buildApp}/bin/battlecontrol-ra-zig-build";
+          };
+        in
+        {
+          default = build;
+          inherit build;
+        };
+
+      packages.${system}.default = buildApp;
+    };
+}
