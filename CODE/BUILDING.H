c2a82d3269d1666048c432f2aaad24b88c790b1b
Hugh O'Brien <github@hughobrien.ie>
2026-05-30 23:06:28 -0700

    port: make INI section names const (#85)

--- CODE/BUILDING.H
+++ CODE/BUILDING.H
@@ -343,7 +343,7 @@ class BuildingClass : public TechnoClass
 		*/
 		static void Read_INI(CCINIClass & ini);
 		static void Write_INI(CCINIClass & ini);
-		static char *INI_Name(void) {return "STRUCTURES";};
+		static char const *INI_Name(void) {return "STRUCTURES";};
 		bool Load(Straw & file);
 		bool Save(Pipe & file) const;
 
