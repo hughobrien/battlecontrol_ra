ae2e0f4327e9f029c5880b76e300b63314e5571e
Hugh O'Brien <mail@hughobrien.ie>
2026-05-31 21:34:31 -0700

    Port shape buffer globals to Zig

--- build.zig
+++ build.zig
@@ -312,6 +312,17 @@ pub fn build(b: *std.Build) void {
     });
     exe.root_module.addObject(wwlib_draw_buffer);
 
+    const wwlib_shape_buffer_module = b.createModule(.{
+        .root_source_file = b.path("linux-compat/wwlib/shape_buffer.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const wwlib_shape_buffer = b.addObject(.{
+        .name = "wwlib-shape-buffer",
+        .root_module = wwlib_shape_buffer_module,
+    });
+    exe.root_module.addObject(wwlib_shape_buffer);
+
     const game_algorithms_module = b.createModule(.{
         .root_source_file = b.path("linux-compat/game_algorithms.zig"),
         .target = target,
